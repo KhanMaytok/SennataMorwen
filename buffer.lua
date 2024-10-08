c3d2eabb072aaf011e52b4326fb72cf7b9cc75b3
Giancarlo Ventura <khan.maytok@gmail.com>
2024-10-08 02:25:00 -0500

    Changes on rider behaviour, tune gitatributes

--- buffer.lua
+++ buffer.lua
@@ -72,3 +72,78 @@
 				}
 			}
 		}
+
+
+		random_list = {
+			10 = {
+				scope:attacker.capital_province = {
+					create_epidemic_outbreak = {
+						type = smallpox
+						intensity = minor
+					}
+				}										
+			}
+			10 = {
+				scope:attacker.capital_province = {
+					create_epidemic_outbreak = {
+						type = typhus
+						intensity = minor
+					}
+				}										
+			}
+			10 = {
+				scope:attacker.capital_province = {
+					create_epidemic_outbreak = {
+						type = peryite_affliction
+						intensity = minor
+					}
+				}										
+			}
+			10 = {
+				scope:attacker.capital_province = {
+					create_epidemic_outbreak = {
+						type = chrondiasis
+						intensity = minor
+					}
+				}										
+			}
+			10 = {
+				scope:attacker.capital_province = {
+					create_epidemic_outbreak = {
+						type = dysentery
+						intensity = minor
+					}
+				}										
+			}
+			10 = {
+				scope:attacker.capital_province = {
+					create_epidemic_outbreak = {
+						type = measles
+						intensity = minor
+					}
+				}										
+			}
+			10 = {
+				scope:attacker.capital_province = {
+					create_epidemic_outbreak = {
+						type = consumption
+						intensity = minor
+					}
+				}										
+			}
+		}
+
+		while = {
+			count = 5
+			spawn_army = {
+				uses_supply = no
+				inheritable = no
+				name = "risen_dead"
+				levies = {
+					value = {
+						value = 750
+					}
+				}
+				location = scope:acs_hecate_province
+			}
+		}
