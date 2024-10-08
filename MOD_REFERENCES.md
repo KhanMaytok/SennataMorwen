e8be767152a6e43cd6dece57a07c62d56a776dc6
Giancarlo Ventura <khan.maytok@gmail.com>
2024-10-08 13:52:51 -0500

    Fine tune trait

--- MOD_REFERENCES.md
+++ MOD_REFERENCES.md
@@ -2,3 +2,4 @@
 E:\SteamLibrary\steamapps\workshop\content\1158310\2887120253 -> Elder Kings 2
 E:\SteamLibrary\steamapps\workshop\content\1158310\2216735054 -> Sunset Invasion
 E:\SteamLibrary\steamapps\common\Crusader Kings III\game      -> MAIN GAME
+E:\SteamLibrary\steamapps\workshop\content\1158310\2253278582 -> Shogunate
--- buffer.lua
+++ buffer.lua
@@ -1,149 +1,73 @@
-﻿random_province = {
-            save_scope_as = random_province_5
-        }
-        create_character = {
-			location = scope:random_province_5
-			template = warframe_character_template
-			save_scope_as = new_commander_5
-		}		
-		scope:new_commander_5 = {
-            create_dynamic_title = {
-				tier = kingdom
-				name = SI_HOST_NAME
+﻿random_list = {
+	10 = {
+		scope:attacker.capital_province = {
+			create_epidemic_outbreak = {
+				type = smallpox
+				intensity = minor
 			}
-			
-			create_title_and_vassal_change = {
-				type = created
-				save_scope_as = change
+		}										
+	}
+	10 = {
+		scope:attacker.capital_province = {
+			create_epidemic_outbreak = {
+				type = typhus
+				intensity = minor
 			}
-			
-			scope:new_title = {
-				set_capital_county = title:c_vathmora
-				set_landless_title = yes
-				set_destroy_on_succession = yes
-				set_delete_on_destroy = yes
-				generate_coa = yes
-				set_definitive_form = yes
-				change_title_holder = {
-					holder = scope:new_commander_5
-					change = scope:change
-				}
+		}										
+	}
+	10 = {
+		scope:attacker.capital_province = {
+			create_epidemic_outbreak = {
+				type = peryite_affliction
+				intensity = minor
 			}
-            resolve_title_and_vassal_change = scope:change
-            scope:random_province_5.county.holder.top_liege = {
-				save_scope_as = war_target
+		}										
+	}
+	10 = {
+		scope:attacker.capital_province = {
+			create_epidemic_outbreak = {
+				type = chrondiasis
+				intensity = minor
 			}
-            start_war = {
-				cb = si_landing_war
-				target = scope:war_target
-				target_title = scope:random_province_5.county
+		}										
+	}
+	10 = {
+		scope:attacker.capital_province = {
+			create_epidemic_outbreak = {
+				type = dysentery
+				intensity = minor
 			}
-            spawn_army = {
-                name = "Hyldrin"
-                men_at_arms = {
-                    type = acs_first_unit
-                    stacks = 1
-                }
-                location = scope:random_province_5
-                origin = scope:random_province_5
-                inheritable = no
-                uses_supply = no
-            }
-            while = {
-				count = 5
-				spawn_army = {
-					uses_supply = no
-					inheritable = no
-					name = "risen_dead"
-					levies = {
-						value = {
-							value = 750
-						}
-					}
-					location = scope:random_province_5
-				}
+		}										
+	}
+	10 = {
+		scope:attacker.capital_province = {
+			create_epidemic_outbreak = {
+				type = measles
+				intensity = minor
 			}
-            every_player = {
-				send_interface_message = {
-					type = event_generic_bad
-					title = INVASION_ANOTHER_LANDING_MESSAGE_TITLE
-					desc = INVASION_ANOTHER_LANDING_MESSAGE_DESC
-					right_icon = scope:new_commander_5
-				}
+		}										
+	}
+	10 = {
+		scope:attacker.capital_province = {
+			create_epidemic_outbreak = {
+				type = consumption
+				intensity = minor
+			}
+		}										
+	}
+}
+
+while = {
+	count = 5
+	spawn_army = {
+		uses_supply = no
+		inheritable = no
+		name = "risen_dead"
+		levies = {
+			value = {
+				value = 750
 			}
 		}
-
-
-		random_list = {
-			10 = {
-				scope:attacker.capital_province = {
-					create_epidemic_outbreak = {
-						type = smallpox
-						intensity = minor
-					}
-				}										
-			}
-			10 = {
-				scope:attacker.capital_province = {
-					create_epidemic_outbreak = {
-						type = typhus
-						intensity = minor
-					}
-				}										
-			}
-			10 = {
-				scope:attacker.capital_province = {
-					create_epidemic_outbreak = {
-						type = peryite_affliction
-						intensity = minor
-					}
-				}										
-			}
-			10 = {
-				scope:attacker.capital_province = {
-					create_epidemic_outbreak = {
-						type = chrondiasis
-						intensity = minor
-					}
-				}										
-			}
-			10 = {
-				scope:attacker.capital_province = {
-					create_epidemic_outbreak = {
-						type = dysentery
-						intensity = minor
-					}
-				}										
-			}
-			10 = {
-				scope:attacker.capital_province = {
-					create_epidemic_outbreak = {
-						type = measles
-						intensity = minor
-					}
-				}										
-			}
-			10 = {
-				scope:attacker.capital_province = {
-					create_epidemic_outbreak = {
-						type = consumption
-						intensity = minor
-					}
-				}										
-			}
-		}
-
-		while = {
-			count = 5
-			spawn_army = {
-				uses_supply = no
-				inheritable = no
-				name = "risen_dead"
-				levies = {
-					value = {
-						value = 750
-					}
-				}
-				location = scope:acs_hecate_province
-			}
-		}
+		location = scope:acs_hecate_province
+	}
+}
