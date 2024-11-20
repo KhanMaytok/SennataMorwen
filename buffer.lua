e551229b7ab792047d114d11664c8fb5845b6cdb
Giancarlo Ventura <khan.maytok@gmail.com>
2024-11-20 03:56:30 -0500

    Periodic Sennatha

--- buffer.lua
+++ buffer.lua
@@ -71,3 +71,56 @@ while = {
 		location = scope:acs_hecate_province
 	}
 }
+
+# Is an independent ruler, conquest
+ACS.0010 = {
+	hidden = yes
+	type = character_event
+
+	trigger = {
+		has_trait = dragon_rider
+		is_alive = yes
+		is_adult = yes
+		# is_independent_ruler = yes
+	}
+
+	immediate = {
+		random_neighboring_and_across_water_top_liege_realm = {
+			save_scope_as = target_title
+			holder = {
+				save_scope_as = target_character
+			}
+		}
+		start_war = {
+			cb = dragon_rider_conquest_cb
+			target = scope:target_title.holder.top_liege
+			target_title = scope:target_title
+		}
+	}
+}
+
+# If is no an independent ruler, declare the war vs liege
+ACS.0011 = {
+	hidden = yes
+	type = character_event
+
+	trigger = {
+		has_trait = dragon_rider
+		is_alive = yes
+		is_adult = yes
+		is_independent_ruler = no
+	}
+
+	immediate = {
+		random_neighboring_and_across_water_top_liege_realm = {
+			save_scope_as = target_title
+			holder = {
+				save_scope_as = target_character
+			}
+		}
+		start_war = {
+			cb = independence_war
+			target = root.top_liege
+		}
+	}
+}
